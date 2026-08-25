da0c33cfbab6aa4c0f16fcd896ea215d8bf9ffd2
ErikSando <eriksando10@gmail.com>
2026-08-25 14:32:41 +0930

    Improved token parsing
    Moved into a seperate function and added support for operators and parenthesis

--- src/evaluator.py
+++ src/evaluator.py
@@ -10,61 +10,65 @@ precedence:
 unary -,
 * / % and implicit multiplication,
 + -
+
+useful links
+https://en.wikipedia.org/wiki/Binary_expression_tree
 """
 
-digits = "0123456789"
+operators = "+-*/%^"
+
+def tokenise(e: str) -> list[list]:
+    tokens = []
+
+    # split into tokens [TYPE : VALUE] end with [END]
+
+    l = len(e)
+    i = 0
+
+    while i < l:
+        char = e[i]
+
+        if char.isdigit():
+            start = i
+
+            while i + 1 < l and e[i + 1].isdigit():
+                i += 1
+
+            if i < l and e[i] == '.':
+                i += 1
+
+                while i + 1 < l and e[i + 1].isdigit():
+                    i += 1
+
+            tokens.append(["NUM", e[start : i + 1]])
+
+        elif char in operators:
+            tokens.append(["OP", char])
+
+        elif char == '(':
+            tokens.append(["LPAREN", char])
+
+        elif char == ')':
+            tokens.append(["RPAREN", char])
+
+        else:
+            if not char.isspace():
+                print(f"Invalid character: '{char}'")
+
+        i += 1
+
+    tokens.append(["END"])
+
+    return tokens
 
 def evaluate_file(input_path: str) -> list[dict]:
     f = open(input_path, "r")
     expressions = f.readlines()
     f.close()
 
-    tokens = []
-
     for e in expressions:
-        # split into tokens [TYPE : VALUE] end with [END]
-
-        value = 0
-        is_number = False
-        in_decimals = False
-        dp = 1
-
-        t_type = "NUM"
-
-        for char in e:
-            if char in digits:
-                if in_decimals:
-                    is_number = True
-                    value += int(char) / (10 ** dp)
-                    dp += 1
-
-                else:
-                    is_number = True
-                    value *= 10
-                    value += int(char)
-
-            elif char == ".":
-                is_number = True
-                in_decimals = True
-
-            else:
-                if is_number:
-                    tokens.append(["NUM", value])
-                    is_number = False
-                    in_decimals = False
-                    value = 0
-                    dp = 1
-
-        if is_number:
-            tokens.append(["NUM", value])
-            is_number = False
-            in_decimals = False
-            value = 0
-            dp = 1
-
-    tokens.append(["END"])
-
-    print(tokens)
+        tokens = tokenise(e)
+        print(*tokens)
 
 while True:
     input_path = input("Input path: ")
