038b50a68689b2543991d0c65469332e4425cf38
ErikSando <eriksando10@gmail.com>
2026-08-26 14:25:01 +0930

    Started on a recursive tree evaluator

--- src/evaluator.py
+++ src/evaluator.py
@@ -54,12 +54,156 @@ def tokenise(e: str) -> list[list]:
         else:
             if not char.isspace():
                 print(f"Invalid character: '{char}'")
+                return ["ERROR"]
 
         i += 1
 
     tokens.append(["END"])
 
     return tokens
+
+def evaluate_tree(node: dict):
+    if node["type"] == "number":
+        return node["value"]
+
+    if node["type"] == "unary":
+        return -node["value"]
+
+    if node["type"] == "operation":
+        if node["op"] == "+":
+            return evaluate_tree(node["left"]) + evaluate_tree(node["right"])
+
+        if node["op"] == "-":
+            return evaluate_tree(node["left"]) - evaluate_tree(node["right"])
+
+        if node["op"] == "*":
+            return evaluate_tree(node["left"]) * evaluate_tree(node["right"])
+
+        if node["op"] == "/":
+            return evaluate_tree(node["left"]) / evaluate_tree(node["right"])
+
+        if node["op"] == "%":
+            return evaluate_tree(node["left"]) % evaluate_tree(node["right"])
+
+        if node["op"] == "^":
+            return evaluate_tree(node["left"]) ** evaluate_tree(node["right"])
+
+#region Ideas and testing
+
+"""
+the goal is to create one large dictionary to be the tree, example:
+
+expression: 3 + 5 * 2
+
+tree = {
+    "type": "operation",
+    "op": "+",
+    "left": {
+        "type": "number",
+        "value": 3
+    },
+    "right": {
+        "type": "operation",
+        "op": "*",
+        "left": {
+            "type": "number",
+            "value": 5
+        },
+        "right": {
+            "type": "number",
+            "value": 2
+        },
+    }
+}
+
+or maybe numbers can be simplified, and the recursive evaluator checks first if it is a plain number
+not sure how to check if a variable is of a number type (int or float)
+
+if we use that approach, the tree would look like:
+
+tree = {
+    "type": "operation",
+    "op": "+",
+    "left": 3,
+    "right": {
+        "type": "operation",
+        "op": "*",
+        "left": 5,
+        "right": 2
+    }
+}
+
+operator precendences and parenthesis need to be considered when constructing the tree
+"""
+
+"""
+EVALUATOR TEST
+expression: 0.4 * 5 ^ 2 + 6 / 2 - 5 % 3
+should be evaluated as
+0.4 * 25 + 6 / 2 - 5 % 3
+10 + 6 / 2 - 5 % 3
+10 + 3 - 5 % 3
+10 + 3 - 2
+13 - 2
+11
+"""
+
+tree = {
+    "type": "operation",
+    "op": "-",
+    "left": {
+        "type": "operation",
+        "op": "+",
+        "left": {
+            "type": "operation",
+            "op": "*",
+            "left": {
+                "type": "number",
+                "value": 0.4
+            },
+            "right": {
+                "type": "operation",
+                "op": "^",
+                "left": {
+                    "type": "number",
+                    "value": 5
+                },
+                "right": {
+                    "type": "number",
+                    "value": 2
+                }
+            }
+        },
+        "right": {
+            "type": "operation",
+            "op": "/",
+            "left": {
+                "type": "number",
+                "value": 6
+            },
+            "right": {
+                "type": "number",
+                "value": 2
+            }
+        }
+    },
+    "right": {
+        "type": "operation",
+        "op": "%",
+        "left": {
+            "type": "number",
+            "value": 5
+        },
+        "right": {
+            "type": "number",
+            "value": 3
+        }
+    }
+}
+
+print("Result:", evaluate_tree(tree))
+
+#endregion
 
 def evaluate_file(input_path: str) -> list[dict]:
     f = open(input_path, "r")
